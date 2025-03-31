517ed9a63046ef71f617f315f7cb265f4707b4a2
karypzhanov-k-auca-2022 <kairat_k@auca.kg>
2025-03-31 20:20:30 +0600

    update application properties and refactor Task class, fix problems

---
+++ src/main/java/kair/example/aop/LoggingAspect.java
@@ -0,0 +1,11 @@
+package kair.example.aop;
+
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+
+
+@Component
+@Aspect
+public class LoggingAspect {
+
+}
